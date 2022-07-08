249721f1ec28f0e68a09db8bbc5694c86ee7814c
Surendra Kumar <88983549+Surendra360@users.noreply.github.com>
2022-07-08 22:44:51 +0530

    Add files via upload

---
+++ AUCTION.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int t,a,b,c;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++)
+    {
+        scanf("%d%d%d",&a,&b,&c);
+        if(a>b && a>c)
+            printf("Alice\n");
+        else if(b>a && b>c)
+            printf("Bob\n");
+        else if(c>a && c>b)
+            printf("Charlie\n");
+        
+    }
+
+
+	return 0;
+}
+
